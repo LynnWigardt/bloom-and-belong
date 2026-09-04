8739de3edad0d13e30db0b4e3bd013ca55ed60fe
Lynn Wigardt <lynn.wigardt@hotmail.com>
2026-09-04 15:37:16 +0200

    Project and database set up

---
+++ html/index.php
@@ -0,0 +1,19 @@
+<?php
+$page_name = "Bloom & Belong";
+?>
+
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $page_name; ?></title>
+</head>
+<body>
+
+<h1><?php echo $page_name; ?></h1>
+
+    <p>En trygg och mysig plats för kvinnor att mötas, dela intressen och hitta gemenskap</p>
+
+</body>
+</html>
