da8786b24ad7b0a3bf4b24f675c6710fd5025bad
Lynn Wigardt <lynn.wigardt@hotmail.com>
2026-09-05 13:25:38 +0200

    Logged in user homepage

--- html/index.php
+++ html/index.php
@@ -1,5 +1,21 @@
 <?php
+
+session_start();
+
+require_once 'db.php';
+
 $page_name = "Bloom & Belong";
+
+$user = null;
+
+if (isset($_SESSION['user_id'])) {
+    $stmt = $pdo->prepare(
+        'SELECT * FROM Users WHERE id = ?'
+    );
+    $stmt->execute([$_SESSION['user_id']]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +30,13 @@ $page_name = "Bloom & Belong";
 <h1><?php echo $page_name; ?></h1>
 
     <p>En trygg och mysig plats för kvinnor att mötas, dela intressen och hitta gemenskap</p>
+
+<?php if ($user): ?>
+
+    <p>Välkommen <?php echo htmlspecialchars($user['first_name']); ?>!</p>
+
+    <?php endif; ?>
+
 
 </body>
 </html>
